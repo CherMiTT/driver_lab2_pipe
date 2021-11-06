c3075a2a311399fcf959fcf6059e30000895cc8e
CherMi <chermi20000@yandex.ru>
2021-11-06 15:28:47 +0300

    Added template for character driver

--- Makefile
+++ Makefile
@@ -3,3 +3,10 @@ obj-m := my_pipe.o
 KDIR := /home/chermi/projects/kernel/linux-stable
 all:
 	$(MAKE) -C $(KDIR) M=$$PWD
+
+check:
+	cppcheck --enable=all --inconclusive --library=posix my_pipe.c
+	/home/chermi/projects/kernel/linux-stable/scripts/checkpatch.pl -f my_pipe.c
+
+test:
+		
---
+++ my_pipe.c
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+
+static int major; //major number
+
+static ssize_t pipe_read(struct file *f, char __user *buf,
+	size_t count, loff_t *offset)
+{
+	pr_info("my_pipe read");
+	return 0;
+}
+
+static ssize_t pipe_write(struct file *f, const char __user *buf,
+	size_t count, loff_t *offset)
+{
+	pr_info("my_pipe write");
+	return 0;
+
+}
+
+static int pipe_open(struct inode *i, struct file *f)
+{
+	pr_info("my_pipe open");
+	return 0;
+}
+
+static int pipe_release(struct inode *i, struct file *f)
+{
+	pr_info("my_pipe release");
+	return 0;
+}
+
+static const struct file_operations fops = {
+	.read = pipe_read,
+	.write = pipe_write,
+	.open = pipe_open,
+	.release = pipe_release,
+	//.unlocked_ioctl = ,
+};
+
+static int __init pipe_init(void)
+{
+	pr_info("Init");
+	major = register_chrdev(0, "my_pipe", &fops);
+	if (major < 0) {
+		pr_crit("failed to register\n");
+		return major;
+	}
+	pr_info("my_pipe assigned major %d\n", major);
+	return 0;
+}
+
+static void __exit pipe_exit(void)
+{
+	pr_info("Exited");
+	unregister_chrdev(major, "my_pipe");
+}
+
+module_init(pipe_init);
+module_exit(pipe_exit);
+
+MODULE_AUTHOR("Chernomorets M.");
+MODULE_DESCRIPTION("A simple implementation of pipe");
+MODULE_LICENSE("GPL");
