da6520a5f03f22191336cf7175c284facdc044d9
CherMi <chermi20000@yandex.ru>
2021-11-16 12:52:38 +0300

    Added copying buffer from user in write

--- my_pipe.c
+++ my_pipe.c
@@ -3,22 +3,37 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/slab.h>
 
 static int major; //major number
+
+
+const size_t BUFFER_SIZE = 1024;
+static char *circular_buffer;
+int read_ptr, write_ptr;
+
 
 static ssize_t pipe_read(struct file *f, char __user *buf,
 	size_t count, loff_t *offset)
 {
-	pr_alert("my_pipe read\n");
+	pr_alert("my_pipe read %lu bytes\n", count);
+
 	return 0;
 }
 
 static ssize_t pipe_write(struct file *f, const char __user *buf,
 	size_t count, loff_t *offset)
 {
-	pr_alert("my_pipe write\n");
-	return 0;
+	pr_alert("my_pipe write %lu bytes\n", count);
+	char *tmp_buf;
+	tmp_buf = kmalloc(count, GFP_KERNEL);
+	//TODO: check memory allocation
+	//TODO: check count against buffer_size
+	copy_from_user(tmp_buf, buf, count);
+	pr_info("write from user: %s\n", tmp_buf);
+	kfree(tmp_buf);
 
+	return 0;
 }
 
 static int pipe_open(struct inode *i, struct file *f)
@@ -50,11 +65,17 @@ static int __init pipe_init(void)
 		return major;
 	}
 	pr_alert("my_pipe assigned major %d\n", major);
+
+	circular_buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL);
+	//TODO: check memory allocation
+	read_ptr = write_ptr = 0;
+
 	return 0;
 }
 
 static void __exit pipe_exit(void)
 {
+	kfree(circular_buffer);
 	unregister_chrdev(major, "my_pipe");
 }
 
