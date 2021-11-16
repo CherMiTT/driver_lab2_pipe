169e571aa08cfb951f0127503e8a224a1bed82bd
CherMi <chermi20000@yandex.ru>
2021-11-16 12:51:59 +0300

    Changed writer to system calls

--- writer.c
+++ writer.c
@@ -1,15 +1,24 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 
 int main()
 {
-	FILE *f = fopen("/dev/my_pipe", "w");
-	if(f == NULL) 
+	int f = open("/dev/my_pipe", O_WRONLY);
+	if(f == -1) 
 	{
 		printf("couldn't open\n");
 		return -1;
 	}
 	printf("opened\n");
-	fclose(f);
+
+	char *str = "Hello world";
+	int w = write(f, str, 12);
+	printf("written %d bytes\n", w);
+
+	close(f);
 	printf("closed\n");
 	return 0;
 }
