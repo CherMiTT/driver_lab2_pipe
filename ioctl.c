86b76a930be0104bc3af62cdf762e4d1a08171d3
CherMi <chermi20000@yandex.ru>
2021-11-20 13:36:35 +0300

    Added int as command line argument

--- ioctl.c
+++ ioctl.c
@@ -4,11 +4,19 @@
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include "my_pipe.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+	if (argc < 2) {
+		printf("Specify new buffer size\n");
+		return -1;
+	}
+
+	int bytes = atol(argv[1]);
+
 	int f = open("/dev/my_pipe", O_RDWR);
 	if(f == -1) {
 		printf("couldn't open\n");
@@ -16,7 +24,8 @@ int main()
 	}
 	printf("opened\n");
 
-	ioctl(f, WR_CAPCITY, 1000);
+	printf("Calling ioctl with %d bytes\n", bytes);
+	ioctl(f, WR_CAPCITY, bytes);
 
 	close(f);
 	printf("closed\n");
